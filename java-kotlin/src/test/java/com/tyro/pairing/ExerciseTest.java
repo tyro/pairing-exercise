eb8923c39620a1b4bb0925972f32a83cb4e69ce6
Stephen O'Rourke <sorourke@tyro.com>
2022-08-16 08:55:31 +1000

    Bump java-kotlin dependencies and configure dependabot

--- java-kotlin/src/test/java/com/tyro/pairing/ExerciseTest.java
+++ java-kotlin/src/test/java/com/tyro/pairing/ExerciseTest.java
@@ -2,14 +2,13 @@ package com.tyro.pairing;
 
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExerciseTest {
 
     @Test
     void shouldFail() {
-        assertThat(true, is(false));
+        assertEquals(true, false);
     }
 
 }
